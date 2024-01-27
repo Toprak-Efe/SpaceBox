e4d5036d6b25f234c9d1ff62c7a439192c189184
Toprak Efe Akkılıç <efe.akkilic@ozu.edu.tr>
2024-01-27 12:30:58 +0300

    Added controls.

--- src/components/SpaceMap.vue
+++ src/components/SpaceMap.vue
@@ -13,6 +13,9 @@ export default {
   data() {
     return {
       model: {
+        camera: {
+          position: new Vector2(0, 0)
+        },
         entities: []
       },
       resources: {
@@ -36,18 +39,30 @@ export default {
         }
 
         // Create player ship
-        this.model.entities.push(new Ship(this.resources.sprites[0], {
+        this.model.entities.push(new Ship(this.resources.sprites[1], {
           position: new Vector2(0, 0),
-          velocity: new Vector2(0, 0),
+          velocity: new Vector2(10.0, 0),
           acceleration: new Vector2(0, 0),
+          omega: 0.0,
           rotation: 0.0
         }));
         this.model.entities[0].pose.position.x = this.canvas.width / 2;
         this.model.entities[0].pose.position.y = this.canvas.height / 2;
 
-        // Listen for mouse pressdown event
-        window.addEventListener('mousedown', this.handleMouseControl);
+        // Create enemy ships
+        for (let i = 0; i < 10; i++) {
+          this.model.entities.push(new Ship(this.resources.sprites[0], {
+            position: new Vector2(0, 0),
+            velocity: new Vector2(0, 0),
+            acceleration: new Vector2(0, 0),
+            rotation: 0.0
+          }));
+          this.model.entities[i + 1].pose.position.x = Math.random() * 1000 - 500;
+          this.model.entities[i + 1].pose.position.y = Math.random() * 1000 - 500;
+          this.model.entities[i + 1].pose.rotation = Math.random() * Math.PI * 2;
+        }
 
+        window.addEventListener('mousedown', this.handleMouseControl);
         window.requestAnimationFrame(this.draw);
       },
       computed: {
@@ -66,6 +81,11 @@ export default {
     draw() {
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+      // Update camera pose.
+      this.model.camera.position.x = this.model.entities[0].pose.position.x;
+      this.model.camera.position.y = this.model.entities[0].pose.position.y;
+
+      // Draw entities.
       for (let entity of this.model.entities) {
         this.drawEntity(entity);
         entity.update(0.01);
@@ -74,22 +94,32 @@ export default {
       window.requestAnimationFrame(this.draw);
     },
     drawEntity(entity) {
-      this.ctx.save();
-      this.ctx.translate(entity.pose.position.x, entity.pose.position.y);
-      // Check if entity is ship or not, if ship, calculate rotation angle from speed vector
       if (entity instanceof Ship) {
-        entity.pose.rotation = Math.atan2(entity.pose.velocity.y, entity.pose.velocity.x) + Math.PI / 2;
+        entity.pose.omega = Math.atan2(entity.pose.velocity.y, entity.pose.velocity.x) + Math.PI / 2;
       }
+
+      console.log("Canvas Dimensions: " + this.canvas.width + "x" + this.canvas.height);
+
+      const x = entity.pose.position.x - this.model.camera.position.x + this.canvas.width/2;
+      const y = entity.pose.position.y - this.model.camera.position.y + this.canvas.height/2;
+      const size = [entity.sprite.size[0], entity.sprite.size[1]];
+
+      this.ctx.save();
+      this.ctx.translate(x, y);
       this.ctx.rotate(entity.pose.rotation);
-      this.ctx.drawImage(entity.sprite.image, -entity.sprite.size[0]/2, -entity.sprite.size[1]/2, entity.sprite.size[0], entity.sprite.size[1]);
+      this.ctx.drawImage(
+        entity.sprite.image,
+        -size[0]/2, -size[1]/2,
+        size[0], size[1])
       this.ctx.restore();
     },
     handleMouseControl(e) {
-      const playerPos = this.model.entities[0].pose.position;
-      const mousePos = new Vector2(e.clientX, e.clientY);
-      const direction = Vector2.subtract(mousePos, playerPos);
-      const acceleration = Vector2.scale(Vector2.normalized(direction), 10.0);
-      this.model.entities[0].accelerate(acceleration);
+      console.log("Button Event!")
+      let mousePos = new Vector2(e.clientX, e.clientY);
+      mousePos.x -= this.canvas.width / 2;
+      mousePos.y -= this.canvas.height / 2;
+      const acceleration = Vector2.scale(mousePos, 2.0);
+      this.model.entities[0].pose.acceleration = acceleration;
     }
   }
 }
--- src/components/classes/Object.js
+++ src/components/classes/Object.js
@@ -6,6 +6,7 @@ class Object {
         position: Vector2,
         velocity: Vector2,
         acceleration: Vector2,
+        omega: 0.0,
         rotation: 0.0
     }
 
@@ -17,6 +18,7 @@ class Object {
     update(dt) {
         this.pose.position = Vector2.add(this.pose.position, Vector2.scale(this.pose.velocity, dt));
         this.pose.velocity = Vector2.add(this.pose.velocity, Vector2.scale(this.pose.acceleration, dt));
+        this.pose.rotation += this.pose.omega * dt;
     }
 
     accelerate(vector2) {
